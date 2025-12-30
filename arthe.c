593daf81e5fde3439325f2d9e932e6ed7e85363f
23p61a05j6 <23p61a05j6@vbithyd.ac.in>
2025-12-30 12:29:32 +0530

    Update arthe.c

--- arthe.c
+++ arthe.c
@@ -1,19 +1,4 @@
-
-%{
-#include <stdio.h>
-#include<string.h>
-%}
-
-
-	[0-9a-zA-Z]+     printf("\nExpression: %s");
-	"="              printf("\nOperator: Equal");
-	"+"              printf("\nOperator: Addition");
-	"-"              printf("\nOperator: Subtraction");
-	"*"              printf("\nOperator: Multiplication");
-	"%"              printf("\nOperator: Modulus"); }
-
-
-
+#include<stdio.h>
 int main() {
     printf("Enter an expression:\n");
     
